422f6d9df636419f12d4e7f29046ac75dda3aaf6
ryantaylor <ryan.taylor9@gmail.com>
2018-03-18 17:12:52 -0400

    Use environment variables for secrets.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,9 +3,9 @@ Paperclip.options[:content_type_mappings] = { rec: 'application/octet-stream' }
 unless Rails.env.test?
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_credentials] = {
-    access_key_id: 'key',
-    secret_access_key: 'key',
-    bucket: 'bucket',
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    bucket: ENV['S3_BUCKET'],
   }
   Paperclip::Attachment.default_options[:s3_region] = 'us-east-1'
   Paperclip::Attachment.default_options[:s3_protocol] = :https
