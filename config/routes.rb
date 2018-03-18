4cd91bf8a9c2627adc168c6eb5872e35abddb1d2
ryantaylor <ryan.taylor9@gmail.com>
2018-03-18 17:13:02 -0400

    Add replay create route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :replays, only: %i[index show]
+  resources :replays, only: %i[index show create]
   resources :players, only: %i[index]
   resources :commands, only: %i[index]
   resources :chat_messages, only: %i[index]
