df5cd74c101128e93ad2d30ada86977264f72796
ryantaylor <ryan.taylor9@gmail.com>
2018-03-23 19:10:45 -0400

    Doorkeeper and auth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resource :auth, only: %i[create], controller: :auth
   resources :replays, only: %i[index show create]
   resources :players, only: %i[index]
   resources :commands, only: %i[index]
