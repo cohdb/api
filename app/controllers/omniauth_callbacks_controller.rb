a6995b819bf4dbc30e2196446c89abee9d253fd4
ryantaylor <ryan.taylor9@gmail.com>
2018-03-23 19:10:11 -0400

    Omniauth login redirect.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,18 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # skip_before_action :verify_authenticity_token, only: :steam
 
-  def passthru
-    raise 'BAD'
-  end
-
   def steam
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in @user, event: :authentication
-      response.headers['Custom-Header'] = 'test here'
-      redirect_to 'http://localhost:4000'
+      # sign_in @user, event: :authentication
+      # raw, enc = Devise.token_generator.generate(self.class, :login_token)
+      token = @user.generate_login_token
+      redirect_to "http://localhost:4000/auth?login_token=#{token}"
       # sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
       # set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
