b5ea9dedd4a759215a5c91b5d4be6a45ab57a541
ryantaylor <ryan.taylor9@gmail.com>
2018-03-22 23:33:41 -0400

    Base omniauth setup.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :steam, 'AF6209B0814D1F2BACED6D6A8BB8819E'
+  config.omniauth :steam, ENV['STEAM_API_KEY']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
