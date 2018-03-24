6a1ed84ff980fa267311f32470f58b94434732c7
ryantaylor <ryan.taylor9@gmail.com>
2018-03-23 22:24:22 -0400

    Fix authorize call.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :doorkeeper_authorized!, only: :me
+  before_action :doorkeeper_authorize!, only: :me
 
   def me
     render json: current_user
