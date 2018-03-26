44b327086382ba057543e1980e6e82992e41963c
ryantaylor <ryan.taylor9@gmail.com>
2018-03-26 08:56:02 -0400

    Preload avatar URLs in controller.

--- app/controllers/replays_controller.rb
+++ app/controllers/replays_controller.rb
@@ -6,12 +6,14 @@ class ReplaysController < ApplicationController
 
   def show
     @replay = policy_scope(Replay).find(params[:id])
+    Player.load_steam_avatar_urls(@replay.players)
     render json: @replay
   end
 
   def create
     authorize Replay.new(permitted_attributes(Replay))
     @replay, @players = Replay.create_from_file(permitted_attributes(Replay))
+    Player.load_steam_avatar_urls(@players)
     render json: @replay
   end
 end
