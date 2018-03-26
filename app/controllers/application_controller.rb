b99dae0f97e96ab782dd52520d5804586ea08f39
ryantaylor <ryan.taylor9@gmail.com>
2018-03-26 00:13:24 -0400

    Render 401 and 403 errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::API
   include Pundit
 
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def doorkeeper_unauthorized_render_options(error)
+    { json: { error: error[:error].description } }
+  end
+
   def render(*args)
     options = args.extract_options!
     subject = options[:json]
-    model = subject.is_a?(ActiveRecord::Relation) ? subject.model : subject.class
-    serializer = "#{model.name}Serializer".constantize
-    options[:json] = serializer.new(subject).serialized_json
+
+    unless subject.is_a?(Hash)
+      model = subject.is_a?(ActiveRecord::Relation) ? subject.model : subject.class
+      serializer = "#{model.name}Serializer".constantize
+      options[:json] = serializer.new(subject).serialized_json
+    end
+
     args << options
     super(*args)
   end
@@ -15,5 +25,9 @@ class ApplicationController < ActionController::API
 
   def current_user
     @current_user ||= User.find(doorkeeper_token[:resource_owner_id]) if doorkeeper_token
+  end
+
+  def not_authorized
+    render json: { error: 'Access is forbidden' }, status: :forbidden
   end
 end
