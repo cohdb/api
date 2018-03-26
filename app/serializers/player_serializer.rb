1d4fdd63241331d53b1c39db30ce701394b91e82
ryantaylor <ryan.taylor9@gmail.com>
2018-03-26 08:55:15 -0400

    Serialize player steam avatar URL.

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,4 @@
 class PlayerSerializer < ApplicationSerializer
   set_type :player
-  attributes :id, :replay_id, :name, :steam_id, :faction, :team, :commander_name, :cpm
+  attributes :id, :replay_id, :name, :steam_id, :steam_avatar_url, :faction, :team, :commander_name, :cpm
 end
