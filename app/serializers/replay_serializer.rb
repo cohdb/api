f63c0ab1e67ec03a4f2301de30649e3f1830fc78
ryantaylor <ryan.taylor9@gmail.com>
2018-03-27 22:39:31 -0400

    Serialize user with replay.

--- app/serializers/replay_serializer.rb
+++ app/serializers/replay_serializer.rb
@@ -2,8 +2,9 @@ class ReplaySerializer < ApplicationSerializer
   set_type :replay
   attributes :id, :user_id, :version, :length, :map_name, :url, :recorded_at, :created_at
   has_many :players
+  belongs_to :user
 
   def include
-    [:players]
+    [:players, :user]
   end
 end
