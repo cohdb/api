f245dfb04c9b2371850b38b4d5439197ff5be5b1
ryantaylor <ryan.taylor9@gmail.com>
2018-04-07 00:44:10 -0400

    Commands and players controller specs.

---
+++ spec/controllers/commands_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe CommandsController do
+  describe '#index' do
+    it 'returns ok' do
+      get :index
+      expect(response).to be_ok
+    end
+
+    it 'uses serializer' do
+      expect(CommandSerializer).to receive(:new).and_call_original
+      get :index
+    end
+
+    it 'uses policy scope' do
+      expect_any_instance_of(CommandPolicy::Scope).to receive(:resolve).and_call_original
+      get :index
+    end
+
+    context 'params' do
+      before do
+        allow_any_instance_of(CommandPolicy::Scope).to receive(:resolve).and_call_original
+      end
+
+      context 'player_id' do
+        it 'uses for_player scope' do
+          expect(Command).to receive(:for_player).with('1').and_call_original
+          get :index, params: { player_id: 1 }
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/players_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe PlayersController do
+  describe '#index' do
+    it 'returns ok' do
+      get :index
+      expect(response).to be_ok
+    end
+
+    it 'uses serializer' do
+      expect(PlayerSerializer).to receive(:new).and_call_original
+      get :index
+    end
+
+    it 'uses policy scope' do
+      expect_any_instance_of(PlayerPolicy::Scope).to receive(:resolve).and_call_original
+      get :index
+    end
+
+    context 'params' do
+      before do
+        allow_any_instance_of(PlayerPolicy::Scope).to receive(:resolve).and_call_original
+      end
+
+      context 'replay_id' do
+        it 'uses for_replay scope' do
+          expect(Player).to receive(:for_replay).with('1').and_call_original
+          get :index, params: { replay_id: 1 }
+        end
+      end
+    end
+  end
+end
