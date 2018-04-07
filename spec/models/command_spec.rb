a825d7965a80fd53d3098d9f00721d320a7a193a
ryantaylor <ryan.taylor9@gmail.com>
2018-04-06 22:44:02 -0400

    Command scope and method specs.

--- spec/models/command_spec.rb
+++ spec/models/command_spec.rb
@@ -114,4 +114,105 @@ RSpec.describe Command do
       should be_valid
     end
   end
+
+  describe 'scopes' do
+    describe 'for_player' do
+      let!(:command) { create(:command) }
+      subject { described_class.for_player(player) }
+
+      before do
+        create(:command)
+      end
+
+      context 'nil input' do
+        let(:player) { nil }
+
+        it 'returns all players' do
+          expect(subject.count).to eq(2)
+        end
+      end
+
+      context 'player input' do
+        let(:player) { command.player }
+
+        it 'returns all players for that player' do
+          expect(subject).to contain_exactly(command)
+        end
+      end
+
+      context 'player ID input' do
+        let(:player) { command.player_id }
+
+        it 'returns all players for that player' do
+          expect(subject).to contain_exactly(command)
+        end
+      end
+    end
+  end
+
+  describe '.entity_name' do
+    let(:command) { build(:command, entity_id: entity_id) }
+    subject { command.entity_name }
+
+    context 'entity ID is zero' do
+      let(:entity_id) { 0 }
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'entity ID is known' do
+      let(:entity_id) { 1 }
+
+      before do
+        allow(CommandTypes).to receive(:entity_name_functions).and_return({ command.command_type => proc { |id| "Entity #{id}" } })
+      end
+
+      it 'returns calculated entity name' do
+        expect(subject).to eq('Entity 1')
+      end
+    end
+
+    context 'entity ID is unknown' do
+      let(:entity_id) { 1 }
+
+      before do
+        allow(CommandTypes).to receive(:entity_name_functions).and_return({})
+      end
+
+      it 'returns unknown placeholder' do
+        expect(subject).to eq('UNHANDLED 1')
+      end
+    end
+  end
+
+  describe '.command_text' do
+    let(:command) { build(:command, command_type: command_type, x: 1, y: 2, z: 3) }
+    subject { command.command_text }
+
+    before do
+      allow(command).to receive(:entity_name).and_return('entity')
+    end
+
+    context 'command type is known' do
+      let(:command_type) { 'known' }
+
+      before do
+        allow(CommandTypes).to receive(:command_text_functions).and_return({ command_type => proc { |c| "Issues #{c.command_type} with #{c.entity_name}" } })
+      end
+
+      it 'returns calculated command text' do
+        expect(subject).to eq('Issues known with entity')
+      end
+    end
+
+    context 'command type is unknown' do
+      let(:command_type) { 'unknown' }
+
+      it 'returns unknown placeholder' do
+        expect(subject).to eq('Issues unknown with entity at 1.0 2.0 3.0')
+      end
+    end
+  end
 end
