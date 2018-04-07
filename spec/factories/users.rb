6ef6c14abfa11358f97b019dd0e11805f88afe07
ryantaylor <ryan.taylor9@gmail.com>
2018-04-07 02:38:12 -0400

    Replay policy specs.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -28,7 +28,7 @@
 FactoryBot.define do
   factory :user do
     provider 'steam'
-    uid Faker::Number.number(17).to_s
-    name Faker::ParksAndRec.character
+    uid { Faker::Number.number(17).to_s }
+    name { Faker::ParksAndRec.character }
   end
 end
---
+++ spec/policies/replay_policy_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe ReplayPolicy do
+  subject { described_class }
+
+  let(:anon_replay) { build(:replay) }
+  let(:linked_replay) { build(:replay, user_id: user.id) }
+  let(:user) { create(:user) }
+
+  permissions :create? do
+    it 'allows logged out users to create anonymous replays' do
+      should permit(nil, anon_replay)
+    end
+
+    it 'does not allow logged out users to create linked replays' do
+      should_not permit(nil, linked_replay)
+    end
+
+    it 'allows logged in users to link replays to themselves' do
+      should permit(user, linked_replay)
+    end
+
+    it 'does not allow logged in users to link replays to other users' do
+      should_not permit(user, build(:replay, user_id: create(:user).id))
+    end
+
+    it 'does not allow logged in users to create anonymous replays' do
+      should_not permit(user, anon_replay)
+    end
+  end
+end
