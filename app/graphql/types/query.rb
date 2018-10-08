5df2b67614889b8f1c03ed1087f4bd16a51f4f5f
ryantaylor <ryan.taylor9@gmail.com>
2018-10-07 20:52:37 -0400

    Use find record resolver in query type.

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,18 +1,4 @@
+# frozen_string_literal: true
 class Types::Query < Types::BaseObject
-
-  field :replays, [Types::Replay], null: false do
-    argument :limit, Int, required: false, default_value: 25
-  end
-
-  def replays(limit:)
-    ::Replay.all.includes(:players).limit(limit)
-  end
-
-  field :replay, Types::Replay, null: false do
-    argument :id, ID, required: true
-  end
-
-  def replay(id:)
-    ::Replay.find(id)
-  end
+  field :replay, resolver: Resolvers::FindRecord[Replay]
 end
