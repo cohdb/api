a30c1b40b0f9dd1cab961d504b720a7c5a33ce60
ryantaylor <ryan.taylor9@gmail.com>
2018-10-07 20:50:42 -0400

    Add create replay mutation to type.

--- app/graphql/types/mutation.rb
+++ app/graphql/types/mutation.rb
@@ -1,9 +1,4 @@
+# frozen_string_literal: true
 class Types::Mutation < Types::BaseObject
-
-  # TODO: Remove me
-  field :test_field, String, description: "An example field added by the generator", null: true
-
-  def test_field
-    "Hello World!"
-  end
+  field :create_replay, mutation: Mutations::CreateReplay
 end
