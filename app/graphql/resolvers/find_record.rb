d3c555401a048d050fa82f1d11f7025ec909dd28
ryantaylor <ryan.taylor9@gmail.com>
2018-11-10 14:01:00 -0500

    Clean up find record resolver.

--- app/graphql/resolvers/find_record.rb
+++ app/graphql/resolvers/find_record.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 module Resolvers
   class FindRecord < Base
-    def self.[](type_name)
-      Class.new(Base) do
-        @@model_name = type_name.name.demodulize
-
-        type type_name, null: false
-
-        argument :id, ID, required: true
-
-        description "Find a specific `#{@@model_name}` by `id`"
-
-        def resolve(id:)
-          @@model_name.constantize.find(id)
-        end
+    def self.[](model_class)
+      resolver = Class.new(Base)
+      resolver.type("Types::#{model_class}Type".safe_constantize, null: false)
+      resolver.argument(:id, ID, required: true)
+      resolver.description("Find a specific `#{model_class.name.demodulize}` by `id`")
+      resolver.define_method(:resolve) do |id:|
+        record = model_class.find(id)
+        authorize(record, :show?)
       end
+
+      resolver
     end
   end
 end
