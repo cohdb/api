ac7186de8af078bb2b3297f594c58cdf250cdcbe
ryantaylor <ryan.taylor9@gmail.com>
2018-11-11 10:26:19 -0500

    Add paging strategy to list records scoping.

--- app/graphql/resolvers/list_records.rb
+++ app/graphql/resolvers/list_records.rb
@@ -9,7 +9,8 @@ module Resolvers
         attach_description(resolver, model_class)
         attach_arguments(resolver, model_class)
 
-        define_strategy(resolver, model_class)
+        define_filtering_strategy(resolver, model_class)
+        define_paging_strategy(resolver, model_class)
         define_resolve(resolver, model_class)
 
         resolver
@@ -18,7 +19,7 @@ module Resolvers
       protected
 
       def attach_type(resolver, model_class)
-        resolver.type(["Types::#{model_class}Type".constantize], null: false)
+        resolver.type("Types::#{model_class}ListType".constantize, null: false)
       end
 
       def attach_description(resolver, model_class)
@@ -26,26 +27,49 @@ module Resolvers
       end
 
       def attach_arguments(resolver, model_class)
+        resolver.argument(:first, Integer, required: false, default_value: 10, prepare: ->(first, _) { [first, 50].min })
+        resolver.argument(:cursor, GraphQL::Types::ID, required: false)
+
         transform = Graphql::ModelTransformationService.new(model_class, permitted_attributes_for(model_class, :index))
         transform.attach_arguments_to(resolver)
       end
 
-      def define_strategy(resolver, model_class)
-        resolver.define_method(:strategy) do |params|
-          strategy_class = "#{model_class}::ListRecordsStrategy".safe_constantize
+      def define_filtering_strategy(resolver, model_class)
+        resolver.define_method(:filtering_strategy) do |params|
+          strategy_class = "#{model_class}::ListRecordsFilteringStrategy".safe_constantize
           if strategy_class.nil?
-            ListRecordsStrategy.new(model_class, params)
+            ListRecordsFilteringStrategy.new(model_class, params)
           else
             strategy_class.new(params)
           end
         end
-        resolver.send(:private, :strategy)
+        resolver.send(:private, :filtering_strategy)
+      end
+
+      def define_paging_strategy(resolver, model_class)
+        resolver.define_method(:paging_strategy) do |params|
+          strategy_class = "#{model_class}::ListRecordsPagingStrategy".safe_constantize
+          if strategy_class.nil?
+            ListRecordsPagingStrategy.new(params)
+          else
+            strategy_class.new(params)
+          end
+        end
+        resolver.send(:private, :paging_strategy)
       end
 
       def define_resolve(resolver, model_class)
         resolver.define_method(:resolve) do |**params|
           authorize(model_class, :index?)
-          strategy(params).apply_to(policy_scope(model_class))
+          scope = policy_scope(model_class)
+          scope = filtering_strategy(params).apply_to(scope)
+
+          paging_strategy = paging_strategy(params)
+          scope = paging_strategy.apply_to(scope)
+          {
+            records: scope,
+            meta: { cursor: paging_strategy.cursor_for(scope) }
+          }
         end
       end
     end
