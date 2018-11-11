fc7e4eff6bffb39d5e2848a9909083255a5743c8
ryantaylor <ryan.taylor9@gmail.com>
2018-11-11 10:21:52 -0500

    Move permitted attributes helper to application strategy.

--- app/strategies/application_strategy.rb
+++ app/strategies/application_strategy.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 class ApplicationStrategy
+  include Pundit
+
   def apply_to
     raise NotImplementedError
+  end
+
+  protected
+
+  def permitted_attributes_for(model_class, action)
+    policy = Pundit.policy(nil, model_class)
+    method_name = if policy.respond_to?("permitted_attributes_for_#{action}")
+                    "permitted_attributes_for_#{action}"
+                  else
+                    'permitted_attributes'
+                  end
+    policy.public_send(method_name)
   end
 end
